44facea9373938e4f540fa0d8e8e5bae9ab62867
Raihan <raihanwahid16.com>
2026-07-05 03:04:10 +0600

    initial commit

--- obj/Debug/net10.0/asp-net-ecommerce-web-api.AssemblyInfo.cs
+++ obj/Debug/net10.0/asp-net-ecommerce-web-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("asp-net-ecommerce-web-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f5623904a3728c51394683849642045ffe1923")]
 [assembly: System.Reflection.AssemblyProductAttribute("asp-net-ecommerce-web-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("asp-net-ecommerce-web-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
